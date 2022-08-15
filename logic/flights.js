4725e8e71143b730a29e1255244245153b0c7277
Rhobar9 <olo.k97@wp.pl>
2022-08-15 23:19:41 +0200

    Module1_attempt

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,47 @@
 function Flights() {
-
+	
+	function calculateNumberOfFlights(passengers, capacity){
+		let flights;
+		
+		if ((passengers < 0) || (!Number.isInteger(Number(passengers)))){
+			throw new Error("The number of passengers must be a positive integer value")
+		}
+		
+		if ((capacity < 0) || (!Number.isInteger(Number(capacity)))){
+			throw new Error("The capacity of the flight must be a positive integer value")
+		}
+		
+		if (passengers % capacity == 0){
+			flights = passengers/capacity;
+		} else {
+			flights = Math.floor(passengers/capacity) + 1;
+		}
+		
+		return flights;
+	}
+	
+	function checkAircraftRevision (distanceLimit, distanceArray) {
+		let totalDistance = 0;
+		let distance;
+		
+		for (distance of distanceArray) {
+			totalDistance += distance;
+		}
+		
+		if (totalDistance > distanceLimit) {
+			throw new Error("Flight minimum allowed distance (" + distanceLimit + ") exceeded. No flight is allowed any longer. You need to make the revision immediately.")
+		}
+		
+		if (totalDistance <= distanceLimit/2) {
+			return "The revision needs to be done within the next 3 months";
+		} else if (totalDistance <= * distanceLimit/4) {
+			return "The revision needs to be done within the next 2 months";
+		} else {
+			return "The revision needs to be done within the next month";
+		}
+	}
+	
+	return {calculateNumberOfFlights, checkAircraftRevision};
 }
 
+module.exports = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,68 @@
 function Passengers() {
 
+	function checkFlightCapacity (flightCapacity, passengersArray) {
+		let passengersNumber;
+		let passengers;
+		
+		for(passengers of passengersArray) {
+			passengersNumber += passengers;
+		}
+		
+		if (passengersNumber > flightCapacity) {
+			throw new Error("Flight capacity (" + flightCapacity + ") exceeded. You have" + passengersNumber + " passengers")
+		}
+		return passengersNumber;
+	}
+	
+	function distributeAllSeatsToAllPassengers (vipPassengers, regularPassengers, nrOfFlights, businessSeatsPerFliglight, economySeatsPerFlight) {
+		let vipPassengersWithEconomySeats = 0;
+		let vipPassengersWithBusinessSeats = 0;
+		let regularPassengersWithBusinessSeats = 0;
+		let regularPassengersWithEconomySeats = 0;
+		let availableBusinessSeats = nrOfFlights * businessSeatsPerFliglight;
+		let availableEconomySeats = nrOfFlights * economySeatsPerFlight;
+		
+		var vipBusinessConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats};
+		vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFliglight)
+		
+		var vipEconomyConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats}
+		vipPassengersWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight)
+		
+		var regularBussinesConfiguration = {passengers:regularPassengers, seats:vipBusinessConfiguration.seats}
+		regularPassengersWithBusinessSeats = updateConfiguration(regularBussinesConfiguration, businessSeatsPerFliglight)
+		
+		var regularEconomyConfiguration = {passengers:regularBussinesConfiguration.passengers, seats:vipEconomyConfiguration.seats}
+		regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight)
+		
+		return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats, vipPassengersWithEconomySeats:vipPassengersWithEconomySeats, regularPassengersWithEconomySeats:regularPassengersWithEconomySeats, regularPassengersWithBusinessSeats: regularPassengersWithBusinessSeats
+		}
+	}
+	function updateConfiguration (configuration, seatsPerFlight) {
+		let passengersWithSeats = 0;
+		
+		while (configuration.passengers > 0) {
+			if (configuration.seats > 0) {
+				if (configuration.passengers >= configuration.seats) {
+					configuration.passengers -= seatsPerFlight;
+					configuration.seats -= seatsPerFlight;
+					passengersWithSeats += seatsPerFlight;
+				} else {
+					configuration.passengers -= configuration.seats;
+					passengersWithSeats += configuration.seats;
+					configuration.seats = 0;
+				}
+			} else {
+				passengersWithSeats += configuration.passengers;
+				configuration.seats -= configuration.passengers;
+				configuration.passengers = 0;
+			}
+		} else {
+			break;
+		}
+		return passengersWithSeats;
+	}
+
+return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
 }
 
+module.exports = Passengers();
